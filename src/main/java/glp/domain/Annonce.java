f870fc5ca6f87a56e0b14d22718ee3c270a5033c
aynnas <terry.mainvis@gmail.com>
2014-11-30 01:16:16 +0100

    test premier commit

--- src/main/java/glp/domain/Annonce.java
+++ src/main/java/glp/domain/Annonce.java
@@ -6,7 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 @Entity
 public class Annonce {
--- src/main/java/glp/domain/Categorie.java
+++ src/main/java/glp/domain/Categorie.java
@@ -4,12 +4,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 public class Categorie {
-	
-
 	
 	@Id
 	@GeneratedValue
