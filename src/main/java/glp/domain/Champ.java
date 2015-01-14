561bf11b72d0a9f6f82f9f05db115e86607e42bf
thomas <th.deparis@gmail.com>
2015-01-14 15:20:46 +0100

    m- champ ajout d'un set de champs complétés

--- src/main/java/glp/domain/Champ.java
+++ src/main/java/glp/domain/Champ.java
@@ -1,11 +1,14 @@
 package glp.domain;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Champ {
@@ -19,19 +22,26 @@ public class Champ {
 	@JoinColumn(name="cat_id")
 	private Categorie cat;
 	
+	@OneToMany(mappedBy="champ")
+	private Set<ChampComplete> champscompletes;
+	
 	/**
 	 * type du champ (du texte, une description (gros bloc de texte), ou une valeur)
 	 */
 	@Column(name="type")
 	private TypeChampEnum type;
+	
+	@Column(name="nom")
+	private String nom;
 
 	public Champ() {
 		super();
 	}
 
-	public Champ(int id, Categorie cat, TypeChampEnum type) {
+	public Champ(int id, String nom, Categorie cat, TypeChampEnum type) {
 		super();
 		this.id = id;
+		this.nom = nom;
 		this.cat = cat;
 		this.type = type;
 	}
@@ -58,6 +68,22 @@ public class Champ {
 
 	public void setType(TypeChampEnum type) {
 		this.type = type;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Set<ChampComplete> getChampscompletes() {
+		return champscompletes;
+	}
+
+	public void setChampscompletes(Set<ChampComplete> champscompletes) {
+		this.champscompletes = champscompletes;
 	}
 	
 	
