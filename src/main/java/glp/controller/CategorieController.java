df1f41becdcb6676cd1b217633c0e4a843197129
thomas <th.deparis@gmail.com>
2015-01-15 16:06:27 +0100

    m- typechampenum m- categorie controller ajout des fonctions pour ajouter un champ a- champ_form formulaire pour créer un champ

--- src/main/java/glp/controller/CategorieController.java
+++ src/main/java/glp/controller/CategorieController.java
@@ -1,16 +1,25 @@
 package glp.controller;
 
-import java.util.List;
-
 import glp.domain.Annonce;
 import glp.domain.Categorie;
+import glp.domain.Champ;
+import glp.domain.TypeChampEnum;
 import glp.services.AnnonceService;
 import glp.services.CategorieService;
+import glp.services.ChampService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,6 +32,9 @@ public class CategorieController {
 	@Autowired
 	private AnnonceService annonceService;
 	
+	@Autowired
+	private ChampService champService;
+	
 	@RequestMapping("new")
 	public ModelAndView getCatForm(@ModelAttribute Categorie cat) {
 		return new ModelAndView("cat_form");
@@ -31,6 +43,27 @@ public class CategorieController {
 	@RequestMapping("/addCat")
 	public ModelAndView addCategorie(@ModelAttribute Categorie cat) {
 		categorieService.insertRow(cat);
+		return new ModelAndView("redirect:/");
+	}
+	
+	@RequestMapping(value = "newChamp", method=RequestMethod.GET)
+	public ModelAndView getAnnForm(@ModelAttribute Champ champ) {
+		List<Categorie> catlist = categorieService.getList();
+		
+		List<TypeChampEnum> typelist = new ArrayList<TypeChampEnum>();
+		for(int i=0 ; i<TypeChampEnum.values().length ; i++)
+			typelist.add(TypeChampEnum.values()[i]);
+		
+		Map<String, Object> myModel = new HashMap<String, Object>();
+		myModel.put("catlist", catlist);
+		myModel.put("typelist", typelist);
+		
+		return new ModelAndView("champ_form", myModel);
+	}
+	
+	@RequestMapping("/addChamp")
+	public ModelAndView addAnnonce(@ModelAttribute("champ") @Valid Champ champ) {
+		champService.insertRow(champ);
 		return new ModelAndView("redirect:/");
 	}
 
---
+++ src/main/webapp/WEB-INF/pages/champ_form.jsp
@@ -0,0 +1,45 @@
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" href="<c:url value="/resources/css/form.css" />	">
+<title>Creation d'un champ</title>
+</head>
+<body>
+	<%@ include file="../templates/header.jsp"%>
+	<div class=" large-offset-3 large-6 colums panel">
+		<h2>Ajoutez des champs à compléter dans une annonce !</h2>
+		<form:form id="addChampForm" modelAttribute="champ" method="get"
+			action="addChamp">
+			<div class="row">
+				<div>
+					<form:select path="cat">
+						<form:option value="0" label="Categorie" />
+						<form:options items="${catlist}" itemValue="id" itemLabel="lib" />
+					</form:select>
+				</div>
+			</div>
+			<br/><br/>
+			
+			<div class="row">
+				<form:input type="text" path="nom" placeholder="Nom du champ" />
+			</div>
+			
+			<div>
+				<form:select path="type">
+					<form:option value="0" label="Type de champ" />
+					<form:options items="${typelist}" />
+				</form:select>
+			</div>
+			
+			<input class="button" type="submit" value="Ajouter" />
+
+		</form:form>
+	</div>
+</body>
+</html>
