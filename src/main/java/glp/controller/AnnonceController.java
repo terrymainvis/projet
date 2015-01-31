da49b7db7b44f3b41482c749a19b2474716a2ae0
thomas <th.deparis@gmail.com>
2015-01-31 14:32:20 +0100

    m- annoncecontroller fonctions pour le annonceform (new/categoriechoice, new/form et addAnn)
    il y aura surement des conflits

--- src/main/java/glp/controller/AnnonceController.java
+++ src/main/java/glp/controller/AnnonceController.java
@@ -2,7 +2,11 @@ package glp.controller;
 
 import glp.domain.Annonce;
 import glp.domain.Categorie;
+import glp.domain.ChampComplete;
+import glp.domain.Role;
+import glp.domain.Champ;
 import glp.domain.Utilisateur;
+import glp.form.AnnonceForm;
 import glp.services.AnnonceService;
 import glp.services.CategorieService;
 import glp.services.ChampCompleteService;
@@ -16,25 +20,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.ServletContext;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.ServletContextAware;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/annonce")
-public class AnnonceController implements ServletContextAware{
+public class AnnonceController {
 	@Autowired
 	private AnnonceService annonceService;
 	@Autowired
@@ -45,25 +48,51 @@ public class AnnonceController implements ServletContextAware{
 	private RoleService roleService;
 	@Autowired
 	private ChampCompleteService ccService;
-	
-	private ServletContext servletContext;
 
 	public AnnonceController() {
 	}
 
-
-	// Renvoie sur le formulaire de creation dune annonce en transmettant la
-	// liste des categorie et l'utilisateur courant
-	@RequestMapping(value = "new", method = RequestMethod.GET)
-	public ModelAndView getAnnForm(@ModelAttribute Annonce ann) {
-		Utilisateur utilisateur = utilisateurService.getUserInSession();
-		List<Categorie> catList = categorieService.getList();
+	
+	/**
+	 * pour la création d'une annonce
+	 * 1er form qui demande d'abord la categorie
+	 * @return
+	 */
+	@RequestMapping(value="new/categoriechoice", method=RequestMethod.GET)
+	public ModelAndView getCategorieChoice() {
 		Map<String, Object> myModel = new HashMap<String, Object>();
-		myModel.put("catList", catList);
-		myModel.put("utilisateur", utilisateur);
-		myModel.put("roleList",  roleService.getList());
-		myModel.put("utilisateur", utilisateurService.getUserInSession());
-		return new ModelAndView("ann_form", myModel);
+		myModel.put("catlist", categorieService.getList());
+		myModel.put("annform", new AnnonceForm());
+		
+		return new ModelAndView("ann_form_select_cat", myModel);
+	}
+	
+	/**
+	 * 
+	 * @param annform
+	 * @return le formulaire de création d'une annonce, après que l'on ait choisi une catégorie
+	 */
+	@RequestMapping("new/form")
+	public ModelAndView getForm(@ModelAttribute("annform") AnnonceForm annform) {
+		
+		//si on arrive sur le formulaire sans passer par le choix de catégorie --> redirige
+		if(annform.getCat_choisie()==null)
+			return getCategorieChoice();
+		
+		else {		
+			annform.setAnnonce(new Annonce());
+			annform.getAnnonce().setCategorie(annform.getCat_choisie());
+			annform = ccService.generateForAnnForm(annform);
+			
+			Map<String, Object> myModel = new HashMap<String, Object>();
+			myModel.put("annform", annform);
+			myModel.put("roleList",  roleService.getList());
+			myModel.put("utilisateur", new Utilisateur());
+	//		myModel.put("utilisateur", utilisateurService.getUserInSession());
+			
+			
+			return new ModelAndView("ann_form", myModel);
+		}
 	}
 				
 	
@@ -93,26 +122,60 @@ public class AnnonceController implements ServletContextAware{
 	}
 
 
+	@RequestMapping(value = "new/addAnn", method = RequestMethod.GET)
+	public ModelAndView addAnnonce(@ModelAttribute("annform") /*@Valid*/ AnnonceForm annform/*, BindingResult bindingResult*/) {
 
-	@RequestMapping("/addAnn")/*@RequestParam(value="file", required = false) MultipartFile file*/
+//		if (bindingResult.hasErrors()) {
+//			Utilisateur utilisateur = utilisateurService.getUserInSession();
+//			List<Categorie> catList = categorieService.getList();
+//			Map<String, Object> myModel = new HashMap<String, Object>();
+//			myModel.put("catList", catList);
+//			myModel.put("utilisateur", utilisateur);
+//			System.out.println(bindingResult.getFieldErrorCount());
+//			System.out.println("TEST OK YA DES ERREURS");
+//			return new ModelAndView("ann_form", myModel);
+//		}
+//		
+//		else {
+			Annonce ann_tmp = annform.getAnnonce();
+			
+			//---- A REMETTRE ---
+//			ann_tmp.setAuteur(utilisateurService.getUserInSession());
+		
+			annform.getAnnonce().setAuteur(utilisateurService.getRowById(1));
+			annonceService.insertRow(ann_tmp);
+				 
+			for(ChampComplete cc : ann_tmp.getChampscompletes())
+			{
+				cc.setAnn(ann_tmp);
+				ccService.insertRow(cc);
+			}
+		
+			return new ModelAndView("redirect:/");
+//		}
+	}
+	
+
+/*
+	@RequestMapping("/addAnn") //@RequestParam(value="file", required = false) MultipartFile file
 	public ModelAndView addAnnonce(@ModelAttribute("annonce") @Valid Annonce ann,  @RequestParam("file") MultipartFile file) {
 		ann.setAuteur(utilisateurService.getUserInSession());
 		annonceService.insertRow(ann);
+		
 		System.out.println("fffffahkdaoianjazofffffffff"+file.getName());
 		if (!file.isEmpty()) {
 			 try {
                byte[] bytes = file.getBytes();
                String rootPath = "C:/Users/hadhri/git/projet/src/main/webapp/resources/img";
-               String rootPath1 = servletContext.getRealPath("resources/img");
                // Create the file on server
                String nomImage = file.getName();
                System.out.println(ann.getId()+"dknfsfnsfnslfnsflsnlfdfl");
                System.out.println(nomImage + " ejkeozjoie boh"+file.getName()+"hellooooooooo chat");
-               File serverFile = new File(rootPath1 + File.separator + ann.getId()+".png");
+               File serverFile = new File(rootPath
+                     + File.separator + ann.getId()+".png");
                System.out.println(serverFile);
              BufferedOutputStream stream = new BufferedOutputStream(
                      new FileOutputStream(serverFile));
-             System.out.println("IMAGE CREEE");
              stream.write(bytes);
              stream.close();
 			 }catch(Exception e){
@@ -123,23 +186,13 @@ public class AnnonceController implements ServletContextAware{
 		return new ModelAndView("redirect:/");
 		// return "ann_list";
 	}
+*/
 	
 
-//	@RequestMapping(value = "list", method = RequestMethod.GET)
-//	public ModelAndView getAnnList() {
-//		List<Annonce> annList = annonceService.getList();
-//		List<Categorie> catList = categorieService.getList();
-//		Map<String, Object> myModel = new HashMap<String, Object>();
-//		myModel.put("annList", annList);
-//		myModel.put("catList", catList);
-//		myModel.put("roleList",  roleService.getList());
-//		myModel.put("utilisateur", utilisateurService.getUserInSession());
-//		return new ModelAndView("ann_list",myModel);
-//	}
 	
 	@RequestMapping(value = "list", method = RequestMethod.GET)
-	public ModelAndView getAnnListValides() {
-		List<Annonce> annList = annonceService.getListValides();
+	public ModelAndView getAnnList() {
+		List<Annonce> annList = annonceService.getList();
 		List<Categorie> catList = categorieService.getList();
 		Map<String, Object> myModel = new HashMap<String, Object>();
 		myModel.put("annList", annList);
@@ -173,55 +226,6 @@ public class AnnonceController implements ServletContextAware{
 		myModel.put("roleList",  roleService.getList());
 		myModel.put("utilisateur", utilisateurService.getUserInSession());
 		return new ModelAndView("ann_list", myModel);
-	}
-	
-	@RequestMapping("/{id}/contact")
-	public ModelAndView getContactForm() {
-		System.out.println("okok");
-		return new ModelAndView("contact_form");
-	}
-	
-//	@RequestMapping("/{id}/sendmail")
-//	public ModelAndView sendMail(@PathVariable("id") int idAnnSelected) {
-//		Annonce annonce = annonceService.getRowById(idAnnSelected);
-//		sendMail(annonce.getAuteur().getMailLille1()
-//				, utilisateurService.getUserInSession().getMailLille1()
-//				, "Interet pour l'annonce " + annonce.getId()  + " : " + annonce.getTitre()
-//				,"COUCOU");
-//		System.out.println(annonce.getAuteur().getMailLille1());
-//		System.out.println("test");
-//		return new ModelAndView("redirect:/");
-//	}
-//	
-//	private void sendMail(String mailTo, String mailFrom, String content, String subject){
-//		SimpleMailMessage mail = new SimpleMailMessage()
-//		
-//		mail.setTo(mailTo);
-//		mail.setFrom(mailFrom);
-//		mail.setSubject(subject);
-//		mail.setText(content);
-//		
-//		
-//		
-//		
-//		 MailSender mailSender;
-//		 SimpleMailMessage msg = new SimpleMailMessage();
-//	        msg.setTo(mailTo);
-//	        msg.setText(content);
-//	        try{
-//	            mailSender.send(msg);
-//	        }
-//	        catch (MailException ex) {
-//	            System.err.println(ex.getMessage());
-//	        }
-//	}
-
-
-	@Override
-	public void setServletContext(ServletContext servletContext) {
-		// TODO Auto-generated method stub
-		this.servletContext = servletContext;
-		
 	}
 
 }
